f80c9042f42f6b3ff1a7e70fea3c465ce0ce48f9
Mendenson Mota <mendenson@gmail.com>
2025-01-11 21:33:48 -0700

    primeira atividade adicionada

---
+++ 1.Nivel_basico/1.1.BasicoParte1/1-formatar_poema.py
@@ -0,0 +1,14 @@
+#Imprima o texto seguinte da mesma forma que esta descrita como resultado desejado.
+
+# Texto: "Brilha, brilha, estrelinha, Como eu me pergunto o que você é! Acima do mundo tão alto, Como um diamante no céu. 
+# Brilha, brilha, estrelinha, Como eu me pergunto o que você é!"
+
+# Resultado desejado:
+#"Brilha, brilha, estrelinha,
+#    Como eu me pergunto o que você é! 
+#		 Acima do mundo tão alto,   		
+#		 Como um diamante no céu. 
+# Brilha, brilha, estrelinha, 
+#	 Como eu me pergunto o que você é!"
+
+#----- Escreva seu codigo abaixo ----#
